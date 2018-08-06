524fe7b7d0578b086d6cc2f6865d586791370d2a
Dhivagar Gnanaratnam <dgnanara@uwaterloo.ca>
2018-08-06 01:24:47 -0400

    Update images

--- app/src/main/java/com/example/android/pingpongscorekeeper/adapters/PlayersAdapter.kt
+++ app/src/main/java/com/example/android/pingpongscorekeeper/adapters/PlayersAdapter.kt
@@ -2,13 +2,20 @@ package com.example.android.pingpongscorekeeper.adapters
 
 import android.content.Context
 import android.database.Cursor
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CursorAdapter
 import android.widget.TextView
 import com.example.android.pingpongscorekeeper.R
+import com.example.android.pingpongscorekeeper.R.id.profile_image
 import com.example.android.pingpongscorekeeper.data.PingPongContract
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.players_list_item.view.*
 
 class PlayersAdapter(context: Context, c: Cursor?) : CursorAdapter(context, c, 0) {
 
@@ -20,7 +27,12 @@ class PlayersAdapter(context: Context, c: Cursor?) : CursorAdapter(context, c, 0
         val playerNameView = view.findViewById<TextView>(R.id.usernameTextView)
 
         val setNumberCol = cursor.getColumnIndex(PingPongContract.Player.COLUMN_NAME_TITLE)
+        val setProfileCol = cursor.getColumnIndex(PingPongContract.Player.COLUMN_PROFILE_PICTURE_TITLE)
 
+        if(cursor.getString(setProfileCol) != null) {
+            val bitmap: Bitmap= MediaStore.Images.Media.getBitmap(context.getContentResolver(), Uri.parse(cursor.getString(setProfileCol)) )
+            view.profile_image.setImageBitmap(bitmap)
+        }
         playerNameView.text = cursor.getString(setNumberCol)
 
     }
